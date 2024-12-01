c095d58eae906d6ad83691908c920a07cc74e67e
trangdo149 <trangkoy2k31409@gmail.com>
2024-12-01 21:51:03 +0700

    làm navbar admin

---
+++ Website_Plant/Pages/Admin/Plant/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website_Plant.Pages.Admin.Plant
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
