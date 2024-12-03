22de851678b8eec0dd9a3e6e689773eae661cd49
trangdo149 <trangkoy2k31409@gmail.com>
2024-12-03 23:48:01 +0700

    làm trang qlysp (tiếp)

---
+++ Website_Plant/wwwroot/js/admin.js
@@ -0,0 +1,25 @@
+﻿// Lấy tất cả các liên kết trong menu
+const menuLinks = document.querySelectorAll('.menu-list .menu-item a');
+
+// Lặp qua từng liên kết
+menuLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        // Xóa class menu-item--active khỏi tất cả các mục menu
+        document.querySelectorAll('.menu-item').forEach(item => {
+            item.classList.remove('menu-item--active');
+        });
+
+        // Thêm class menu-item--active vào mục menu được nhấn
+        this.parentElement.classList.add('menu-item--active');
+    });
+});
+
+// Ghi nhớ trạng thái khi tải lại trang
+window.addEventListener('DOMContentLoaded', () => {
+    const currentUrl = window.location.pathname;
+    menuLinks.forEach(link => {
+        if (link.getAttribute('href') === currentUrl) {
+            link.parentElement.classList.add('menu-item--active');
+        }
+    });
+});
