cc57f98af12c5f7ea6976e4951a3e1fe3011acac
Diviw <dinhthaovy03@gmail.com>
2024-12-08 19:54:01 +0700

    signinsignupprofile
    cua Vy

---
+++ Website_Plant/Pages/Auth/profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website_Plant.Pages.Auth
+{
+    public class profileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
