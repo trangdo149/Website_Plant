5ffd48890d8401d88807dbbe8ba41cf3b899acde
pvi1103 <phungvi2003@gmail.com>
2024-12-11 10:39:24 +0700

    oder chua xong

--- Website_Plant/wwwroot/js/site.js
+++ Website_Plant/wwwroot/js/site.js
@@ -1,19 +1,10 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const links = document.querySelectorAll('a');
 
-// Write your JavaScript code.
-document.addEventListener("DOMContentLoaded", () => {
-    const currentPath = window.location.pathname; /
-    const menuLinks = document.querySelectorAll("#nav a"); 
-
-    menuLinks.forEach(link => link.classList.remove("active"));
-
-    menuLinks.forEach(link => {
-        if (link.getAttribute("href") === currentPath) {
-            link.classList.add("active");
-        }
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        // Xóa class 'active' khỏi tất cả các link
+        links.forEach(l => l.classList.remove('active'));
+        // Gán class 'active' vào link được click
+        link.classList.add('active');
     });
-
 });
-
-
