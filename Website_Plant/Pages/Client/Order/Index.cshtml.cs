d462e39cc15126c259248bc0e6f9ee394c3cd2de
trangdo149 <trangkoy2k31409@gmail.com>
2024-12-12 17:44:02 +0700

    sua lai css my order

---
+++ Website_Plant/Pages/Client/Order/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website_Plant.Pages.Client.Order
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Website_Plant/Pages/profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website_Plant.Pages
+{
+    public class profileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
