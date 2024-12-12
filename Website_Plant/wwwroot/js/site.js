16f10d3151d3807d10a721a7c4b51b3dbeb4652c
pvi1103 <phungvi2003@gmail.com>
2024-12-13 00:04:48 +0700

    sửa xong active

--- Website_Plant/wwwroot/js/site.js
+++ Website_Plant/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
-    const currentPath = window.location.pathname; // Đường dẫn hiện tại
-    const allLinks = document.querySelectorAll("a"); // Chọn tất cả liên kết
+    const currentPath = window.location.pathname;
+    const allLinks = document.querySelectorAll("a"); 
 
     // Loại bỏ tất cả class 'active'
     allLinks.forEach(link => link.classList.remove("active"));
