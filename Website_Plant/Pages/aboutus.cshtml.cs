40ed789aa75b3d0a117af82f7b723bcb7a2850d2
thuytrang-0306 <nttt3690@gmail.com>
2024-12-14 22:20:54 +0700

    done about and detail

---
+++ Website_Plant/Pages/aboutus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website_Plant.Pages
+{
+    public class aboutusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
