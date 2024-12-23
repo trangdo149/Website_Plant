0ed260887c41648a2b179bbc555ca5e0c93b69b6
trangdo149 <trangkoy2k31409@gmail.com>
2024-12-23 12:50:53 +0700

    edit profile - done

--- Website_Plant/Pages/profile.cshtml.cs
+++ Website_Plant/Pages/profile.cshtml.cs
@@ -1,12 +1,74 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Data.SqlClient;
+using Website_Plant.MyHelpers;
 
 namespace Website_Plant.Pages
 {
+    [RequireAuth]
+    [BindProperties]
     public class profileModel : PageModel
     {
+        [Required(ErrorMessage = "*Bắt buộc")]
+        public string fullName { get; set; } = "";
+		[Required(ErrorMessage = "*Bắt buộc"), EmailAddress]
+		public string email { get; set; } = "";
+        public string? phone { get; set; } = "";
+		[Required(ErrorMessage = "*Bắt buộc")]
+		public string address { get; set; } = "";
+
+        public string errorMessage = "";
+        public string successMessage = "";
+
         public void OnGet()
         {
+			fullName = HttpContext.Session.GetString("fullName")?? "";
+			email = HttpContext.Session.GetString("email")?? "";
+			phone = HttpContext.Session.GetString("phone");
+			address = HttpContext.Session.GetString("address") ?? "";
+		}
+        public void OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                errorMessage = "Dữ liệu không hợp lệ";
+                return;
+            }
+            if (phone == null) phone = "";
+            string submitButton = Request.Form["action"];
+            string connectionString = "Data Source=Localhost\\sqlexpress;Initial Catalog=WebPlant;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+            if (submitButton.Equals("profile"))
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        string sql = "update users set fullName=@fullName, email=@email, phone=@phone, address=@address where id=@id";
+                        int? id = HttpContext.Session.GetInt32("id");
+                        using (SqlCommand command = new SqlCommand(sql, connection))
+                        {
+                            command.Parameters.AddWithValue("@fullName", fullName);
+							command.Parameters.AddWithValue("@email", email);
+							command.Parameters.AddWithValue("@phone", phone);
+							command.Parameters.AddWithValue("@address", address);
+							command.Parameters.AddWithValue("@id", id);
+                            command.ExecuteNonQuery();
+						}
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorMessage = ex.Message;
+                    return;
+                }
+                HttpContext.Session.SetString("fullName", fullName);
+				HttpContext.Session.SetString("email", email);
+				HttpContext.Session.SetString("phone", phone);
+				HttpContext.Session.SetString("address", address);
+				successMessage = "Cập nhật thành công";
+			}
         }
     }
 }
